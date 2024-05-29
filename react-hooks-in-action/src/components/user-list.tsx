6d7d5024277a6e0c0c5679fbf5e6bd79ada283fb
BiFoS <jon89071@gmail.com>
2024-05-29 16:34:29 +0900

    :art: UseState 도전 과제

--- react-hooks-in-action/src/components/user-list.tsx
+++ react-hooks-in-action/src/components/user-list.tsx
@@ -6,16 +6,31 @@ import { useState } from "react";
 export default function UserList() {
   const users = DB.users;
   const [userIndex, setUserIndex] = useState(1);
+  const user = users[userIndex];
 
   return (
-    <ul className="bookables items-list-nav">
-      {users.map((b, i) => (
-        <li key={b.id} className={i === userIndex ? "selected" : ""}>
-          <button className="btn" onClick={() => setUserIndex(i)}>
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="users items-list-nav">
+        {users.map((u, i) => (
+          <li key={u.id} className={i === userIndex ? "selected" : ""}>
+            <button className="btn" onClick={() => setUserIndex(i)}>
+              {u.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="user-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
